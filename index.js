a4b138027a5d3c7d9a32a17590b3dfd30332cac3
solanawork <businessprivat3@gmail.com>
2023-05-11 21:29:37 +0200

    prototype_final

--- index.js
+++ index.js
@@ -9,9 +9,12 @@ function convertUnits() {
  let meToFe = userInputValue * 3.281
  let liToGa = userInputValue * 0.264
  let kgToP = userInputValue * 2.204
-lengthP.innerHTML =` ${userInputValue} m = ${meToFe.toFixed(3)} feet  `
-volumeP.innerHTML = ` ${userInputValue} liters = ${liToGa.toFixed(3)} gallons`
-massP.innerHTML = `${userInputValue} kg = ${kgToP.toFixed(3)} pounds`
+ let FeToMe = userInputValue / 3.281
+ let GatoLi = userInputValue / 0.264
+ let PtoKg = userInputValue / 2.204
+lengthP.innerHTML =` ${userInputValue} m = ${meToFe.toFixed(3)} feet | ${userInputValue} feet = ${FeToMe.toFixed(3)} m`
+volumeP.innerHTML = ` ${userInputValue} liters = ${liToGa.toFixed(3)} gallons | ${userInputValue} gallons = ${GatoLi.toFixed(3)} liters`
+massP.innerHTML = `${userInputValue} kg = ${kgToP.toFixed(3)} pounds | ${userInputValue} pounds = ${PtoKg.toFixed(3)} kg`
 
 }
 
