b48a75760402d36680f6cdeb738154b14aad68e2
solanawork <businessprivat3@gmail.com>
2023-05-11 21:08:27 +0200

    renderUnits

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
 const renderUnits = document.getElementById("units")
 const fetchUserInput = document.getElementById('user-input')
+const lengthP = document.getElementById("length-p")
+const volumeP = document.getElementById("volume-p")
+const massP = document.getElementById("mass-p")
 function convertUnits() {
 
  let userInputValue = fetchUserInput.value
  let meToFe = userInputValue * 3.281
  let liToGa = userInputValue * 0.264
  let kgToP = userInputValue * 2.204
- renderUnits.innerHTML = 
- `${userInputValue} m = ${meToFe.toFixed(3)} feet <br> 
-                            ${userInputValue} liters = ${liToGa.toFixed(3)} gallons <br>
-                            ${userInputValue} kg = ${kgToP.toFixed(3)} pounds`
+lengthP.innerHTML =` ${userInputValue} m = ${meToFe.toFixed(3)} feet  `
+volumeP.innerHTML = ` ${userInputValue} liters = ${liToGa.toFixed(3)} gallons`
+massP.innerHTML = `${userInputValue} kg = ${kgToP.toFixed(3)} pounds`
 
 }
 
